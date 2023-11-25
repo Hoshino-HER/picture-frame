f07fd4168d3be5749fe702677ac9086f5ca27e55
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2023-11-25 12:02:51 +0900

    first commit

---
+++ src/next.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+  reactStrinctMode: true,
+});
