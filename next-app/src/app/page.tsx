6ba55404f105ae3d2129e3dc0f9f7f3ba7b8f83b
Hoshino Hitoshi <hoshino.hitoshi@sidera.jp>
2023-12-08 09:13:52 +0000

    add menu component

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,22 +1,30 @@
+"use client";
 import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 
-export default function FullScreenDialog() {
+import ImageMenu from '../components/image_menu';
+import { Box } from '@mui/material';
 
+export default function FullScreenDialog() {
   return (
-    <Dialog
-      fullScreen
-      open={true}
-      sx={{ zIndex: 3000 }}
-    >
-      <img
-        alt="Random image"
-        src="https://source.unsplash.com/random"
-        style={{ 
-          width: '100%', height: '100%', 
-          objectFit: 'contain', backgroundColor: 'black'
-        }}
-      />
-    </Dialog>
+    <React.Fragment>
+      <Dialog
+        fullScreen
+        open={true}
+        sx={{ zIndex: -1000 }}
+      >
+        <img
+          alt="Random image"
+          src="https://source.unsplash.com/random"
+          style={{
+            width: '100%', height: '100%', 
+            objectFit: 'contain', backgroundColor: 'black'
+          }}
+        />
+      </Dialog>
+      <Box>
+        <ImageMenu />
+      </Box>
+    </React.Fragment>
   );
 }
